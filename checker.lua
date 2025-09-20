41390096b05b44514885b97d665450355efa2d7b
hydro <123367868+Hydro08@users.noreply.github.com>
2025-09-20 23:00:13 +0800

    Update checker.lua

--- checker.lua
+++ checker.lua
@@ -2,7 +2,7 @@ local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
 local humanoid = char:WaitForChild("Humanoid")
 
-humanoid.BodyHeightScale.Value = 0.5
-humanoid.BodyWidthScale.Value = 0.5
-humanoid.BodyDepthScale.Value = 0.5
-humanoid.HeadScale.Value = 0.5
+humanoid.BodyHeightScale.Value = 0.1
+humanoid.BodyWidthScale.Value = 0.1
+humanoid.BodyDepthScale.Value = 0.1
+humanoid.HeadScale.Value = 0.1
