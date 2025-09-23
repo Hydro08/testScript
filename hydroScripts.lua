dd61ed1fce405340d0a3e10b2b45e2261c57e7ac
hydro <123367868+Hydro08@users.noreply.github.com>
2025-09-23 17:53:04 +0800

    Update hydroScripts.lua

--- hydroScripts.lua
+++ hydroScripts.lua
@@ -16,3 +16,12 @@ if syn and syn.protect_gui then
 end
 
 ScreenGui.Parent = hui
+
+Panel.Parent = ScreenGui
+Panel.Size = UDim2.new(0, 500, 0, 500)
+Panel.Position = UDim2.new(0.5, -110, 0.1, 0)
+Panel.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+Panel.BackgroundTransparency = 0.3
+Panel.BorderSizePixel = 22
+Panel.Active = true
+Panel.Draggable = true
